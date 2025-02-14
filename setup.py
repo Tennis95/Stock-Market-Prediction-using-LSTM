e87ce8ca2ff97baab8112d5285bce31435e525ef
Tennis Raja <118455845+Tennis95@users.noreply.github.com>
2025-02-14 14:17:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 # Load dataset (replace with your file path)
-file_path = "Build"
+file_path = "‎Build/Processed_Stock_Data.xlsx"
 df = pd.read_excel(file_path)
 
 # Preprocess dataset
