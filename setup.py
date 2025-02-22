3dd6aedc16609226484158db647a3947f28dcd6e
Tennis Raja <118455845+Tennis95@users.noreply.github.com>
2025-02-23 00:51:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -118,7 +118,8 @@ uploaded_file = st.sidebar.file_uploader("/content/drive/MyDrive/Processed_Stock
 
 if uploaded_file:
     def load_data(upload_file):
-    df =  load_data(upload_file)
+        return pd.read_excel(filepath = engine"openpyxl") 
+    df =  load_data("/content/drive/MyDrive/Processed_Stock_Data.xlsx")
     df, scaler = preprocess_data(df)
    
 
